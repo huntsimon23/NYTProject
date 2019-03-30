0a28dfbc7c37e8d22cfc3b46e799023a3806c961
huntsimon23 <hunt.simon23@gmail.com>
2019-03-30 11:38:16 -0400

    some Js work

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=O0skeEbeAQACtxV21Y2bNwnfQ9PXZ3fF"
+$.ajax(
+    URL: queryURL,
+    method: "GET",
+)
+
+
+
+});
